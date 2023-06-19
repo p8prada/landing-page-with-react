6e677cb8ebc6c3682297c539e2b3a6fe168e463c
p8prada <131833040+p8prada@users.noreply.github.com>
2023-06-19 23:16:54 +0000

    centering the cards

--- src/js/component/CardList.jsx
+++ src/js/component/CardList.jsx
@@ -4,7 +4,7 @@ const CardList = ({children}) => {
 
 	return (
         <div className="container-fluid">
-            <div className="row d-flex flex-wrap">   
+            <div className="row d-flex flex-wrap d-flex justify-content-center">   
                     {children}
             </div>
         </div>
